dcc641c7929ae808ea90d44454e1a093b68f93e9
Iryna <irasokolova25@ukr.net>
2021-09-08 20:32:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// import './tasks.js/delay.js';
+import './tasks.js/delay.js';
 import './tasks.js/make-transaction.js';
-// import './tasks.js/toggle-user-state.js';
+import './tasks.js/toggle-user-state.js';
          
 
 
